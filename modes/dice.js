0521675d089c5499ed0914a7d00dcdef4ca5d794
Timo Lins <timo.lins@gmail.com>
2016-10-31 11:48:23 +0100

    Customizable dice
    It’s now possible to change the max Number in dice mode.

--- modes/dice.js
+++ modes/dice.js
@@ -2,9 +2,22 @@ import Chance from 'chance'
 
 const chance = new Chance()
 
-export default () => ({
-  title: 'Roll a dice',
-  enabled: true,
-  message: `Dice rolled: *${chance.d6()}*`,
-  icon: 'dice.png'
-})
+export default query => {
+  const defaultN = 6
+  const n = Number(query) || defaultN
+
+  const description = `Number between 1 and ${n}`
+  let disclaimer = ''
+
+  if (n !== defaultN) {
+    disclaimer = `_${description}_\n\n`
+  }
+
+  return {
+    title: 'Roll a dice',
+    enabled: true,
+    message: `${disclaimer}Dice rolled: *${chance.integer({min: 1, max: n})}*`,
+    description,
+    icon: 'dice.png'
+  }
+}
