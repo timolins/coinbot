96ccf9b80e8e3f954ffb8989a7acb869acf240e9
Timo Lins <timo.lins@gmail.com>
2016-11-02 18:43:47 +0100

    Added footer to webapp

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,19 @@ const styles = {
     textAlign: 'center',
     opacity: 0.4,
     fontWeight: 300
+  },
+  header: {
+    marginBottom: 30
+  },
+  footer: {
+    fontSize: 12,
+    marginTop: 5,
+    lineHeight: 1.8,
+    color: 'rgb(100,100,100)'
+  },
+  link: {
+    color: 'white',
+    textDecoration: 'none'
   }
 
 }
@@ -50,12 +63,16 @@ export default class extends React.Component {
           <title>CoinBot</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
-        <header>
+        <header className={style(styles.header)}>
           <img src="static/logo.svg" alt="CoinBot Logo"/>
           <div className={style(styles.title)}>CoinBot</div>
           <div className={style(styles.subtitle)}>for Telegram</div>
         </header>
         <Modal modes={this.props.modes}/>
+        <footer className={style(styles.footer)}>
+          <div>Source available on <a className={style(styles.link)} href="https://github.com/timolins/coinbot">GitHub</a>.</div>
+          <div>Built by <a className={style(styles.link)} href="https://timo.sh">Timo Lins</a>.  Hosted on <a className={style(styles.link)} href="https://zeit.co/now">now</a>.</div>
+        </footer>
       </div>
     )
   }
